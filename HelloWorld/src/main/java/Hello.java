74dddbedda2a4f7290eba6d5305ae1236624025a
ritujava <riitudas@gmail.com>
2021-05-25 11:07:51 -0600

    first commit

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ritu das
+ * @email riitudas@gmail.com
+ * @date 25/05/2021
+ * @purpose My First Hello World Program in IDE
+ * Package com.jritu.helloworld
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
